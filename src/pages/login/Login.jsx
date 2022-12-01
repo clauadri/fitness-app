a17484368956df75438b33220ba7851a41407ac7
angelica <angelica.cavero@gmail.com>
2022-12-01 17:10:08 +0100

    go to register added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Styles.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const goToRegister = () => {
+    navigate("/register");
+  };
+  return (
+    <div>
+      <p>You don´t have an account yet?</p>
+      <button onClick={goToRegister}>Register</button>
+    </div>
+  );
 };
 
 export default Login;
