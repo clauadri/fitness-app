0a93a60b41ac8de03b0245c813ab34641d05ba17
clauadri <adripa1111@gmail.com>
2022-12-03 20:54:59 +0100

    styles fixed and style target delgado

--- src/pages/steps/step-target-body/StepTargetBodyDelgado.jsx
+++ src/pages/steps/step-target-body/StepTargetBodyDelgado.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import nextStep from "../../../shared/nextStep";
-
+import "../Steps.css";
 const StepTargetBody = () => {
 
   const dispatch = useDispatch();
@@ -11,28 +11,32 @@ const StepTargetBody = () => {
  
   
   return (
-    <div>
-    
+    <div className="steps-container">
+      <div>
       <p>Tu ojbetivo es perder peso</p>
       <h2>Elige tu cuerpo deseado</h2>
+      </div>
+      <div className="step-label">
       <label>
-        <input name="delgado" type="radio" value="Delgado" onClick={(ev) => nextStep(ev, dispatch, navigate, '/step-bodytype')}/>
-        <span>
+        <input className="none" name="delgado" type="radio" value="Delgado" onClick={(ev) => nextStep(ev, dispatch, navigate, '/step-bodytype')}/>
+        <div className="step-options">
           <h2>Delgado</h2>
-        </span>
-        <span>
+        </div>
+        <div className="cont-img">
         <img src="https://madmuscles.com/_next/image?url=%2F_next%2Fstatic%2Fassets%2Fgqzsql1QkLs6XYCpGhvxJ%2Fimages%2Fbody-muscles%2FSLIM_BODY.png&w=128&q=75" alt="delgado" />
-        </span>
+        </div>
       </label>
       <label>
-        <input name="delgado-tonificado" type="radio" value="Delgado y tonificado" onClick={(ev) => nextStep(ev, dispatch, navigate, '/step-bodytype')}/>
-        <span>
+        <input className="none" name="delgado-tonificado" type="radio" value="Delgado y tonificado" onClick={(ev) => nextStep(ev, dispatch, navigate, '/step-bodytype')}/>
+        <div className="step-options">
           <h2>Delgado y tonificado</h2>
-        </span>
-        <span>
+        </div>
+        <div className="cont-img">
         <img src="https://madmuscles.com/_next/image?url=%2F_next%2Fstatic%2Fassets%2Fgqzsql1QkLs6XYCpGhvxJ%2Fimages%2Fbody-muscles%2FSLIM_SHREDDED_BODY.png&w=128&q=75" alt="delgado-tonificado" />
-        </span>
+        </div>
       </label>
+      </div>
+      
       
     </div>
     )
