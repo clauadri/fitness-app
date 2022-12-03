44fecd57ddf9203d50aa84c07d878e55de082ea8
Kind <alvaromoreno21197@gmail.com>
2022-12-03 10:47:21 +0100

    FIX app routes cleaner

--- src/App.js
+++ src/App.js
@@ -17,25 +17,16 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/User" element={<User></User>}></Route>
-        <Route path="/step-goal" element={<StepGoal></StepGoal>}></Route>
-        <Route
-          path="/step-target-body-perder-peso"
-          element={<StepTargetBodyDelgado></StepTargetBodyDelgado>}
-        ></Route>
-        <Route
-          path="/step-target-body-crecer-musculo"
-          element={<StepTargetBodyMusculo></StepTargetBodyMusculo>}
-        ></Route>
-        <Route
-          path="/step-target-body-definir"
-          element={<StepTargetBodyDefinir></StepTargetBodyDefinir>}
-        ></Route>
-        <Route path="/step-final" element={<StepFinal/>}/>
-        <Route path="/step-bodytype" element={<StepBodytype/>} />
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/User" element={<User />}/>
+        <Route path="/step-goal" element={<StepGoal />}/>
+        <Route path="/step-target-body-perder-peso" element={<StepTargetBodyDelgado />}/>
+        <Route path="/step-target-body-crecer-musculo" element={<StepTargetBodyMusculo />}/>
+        <Route path="/step-target-body-definir" element={<StepTargetBodyDefinir />}/>
+        <Route path="/step-final" element={<StepFinal />}/>
+        <Route path="/step-bodytype" element={<StepBodytype />} />
       </Routes>
     </div>
   );
