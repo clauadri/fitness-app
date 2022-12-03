6eb386d31693e6a458b3dcc1f719c4cc5b064682
angelica <angelica.cavero@gmail.com>
2022-12-03 17:20:17 +0100

    styles added to main page

--- src/pages/user-info/user/User.jsx
+++ src/pages/user-info/user/User.jsx
@@ -3,16 +3,38 @@ import "./Styles.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logoutUser } from "../../../redux/auth/auth.actions";
+import { useSelector } from "react-redux";
 
 const User = () => {
+  const { user, token } = useSelector((state) => state.auth);
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const logOut = () => {
     dispatch(logoutUser(navigate));
   };
+
+  const Edit = () => {};
+
+  console.log("info", user);
   return (
     <div>
-      <button onClick={logOut}>Logout</button>
+      <div className="user-wrapper">
+        <div>
+          <p>Nombre: {user?.name}</p>
+          <p>Usuario: {user?.user}</p>
+          <p>Email: {user?.email}</p>
+          <p>Edad: {user?.age}</p>
+        </div>
+        <div className="btn-wrapper">
+          <button className="user-btn" onClick={Edit}>
+            Editar
+          </button>
+          <button className="user-btn" onClick={logOut}>
+            Salir
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
