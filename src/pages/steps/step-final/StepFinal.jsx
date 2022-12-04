ab32f3e7c913cddbbbbda21a4893385e9ab10ece
angelica <angelica.cavero@gmail.com>
2022-12-04 13:40:24 +0100

    fix styles

--- src/pages/steps/step-final/StepFinal.jsx
+++ src/pages/steps/step-final/StepFinal.jsx
@@ -1,37 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { getWorkouts } from '../../../redux/workouts/workouts.functions';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getWorkouts } from "../../../redux/workouts/workouts.functions";
 
 const StepFinal = () => {
-
-    const [rutina, setRutina] = useState();
+  const [rutina, setRutina] = useState();
   const dispatch = useDispatch();
 
-  const {workouts, userData, isLoading, error} = useSelector(state => state.workouts);
-
-    useEffect(() => {
-        dispatch(getWorkouts());
-    },[])
+  const { workouts, userData, isLoading, error } = useSelector(
+    (state) => state.workouts
+  );
 
   useEffect(() => {
-    
-  setRutina(workouts.find(workout => {
+    dispatch(getWorkouts());
+  }, []);
 
-    return JSON.stringify(workout.requirements) === JSON.stringify(userData) 
-   })) 
-
-   
-
-  }, [workouts, userData])
+  useEffect(() => {
+    setRutina(
+      workouts.find((workout) => {
+        return (
+          JSON.stringify(workout.requirements) === JSON.stringify(userData)
+        );
+      })
+    );
+  }, [workouts, userData]);
 
   return (
     <div>
-        <div>
-            {console.log('rutina',rutina)}
-            {console.log(userData)}
+      <div>
+        {console.log("rutina", rutina)}
+        {console.log(userData)}
+        <div className="rutine-wrapper">
+          <h1>Rutina</h1>
+          <p>{rutina?.name}</p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StepFinal
+export default StepFinal;
