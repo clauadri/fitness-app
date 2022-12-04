e67ae8dfe9912678b56406df788f030038207116
angelica <angelica.cavero@gmail.com>
2022-12-04 15:02:47 +0100

    fix styles

--- src/pages/user-info/login/Login.jsx
+++ src/pages/user-info/login/Login.jsx
@@ -26,30 +26,32 @@ const Login = () => {
     <div className="login-wrapper">
       <form onSubmit={handleSubmit(send)} className="login">
         <h2>Login</h2>
-        <div>
-          <input
-            placeholder="Email"
-            type="email"
-            name="user"
-            {...register("email", {
-              required: "Introduce tu email, por favor",
-              minLength: {
-                value: 2,
-                message: "el email tiene que ser mas largo",
-              },
-            })}
-          />
-        </div>
-        <div>
-          <input
-            placeholder="Contraseña"
-            type="password"
-            name="password"
-            {...register("password", {
-              required: "El password tiene que existir",
-              // pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/,
-            })}
-          />
+        <div className="inputs-wrapper">
+          <div>
+            <input
+              placeholder="Email"
+              type="email"
+              name="user"
+              {...register("email", {
+                required: "Introduce tu email, por favor",
+                minLength: {
+                  value: 2,
+                  message: "el email tiene que ser mas largo",
+                },
+              })}
+            />
+          </div>
+          <div>
+            <input
+              placeholder="Contraseña"
+              type="password"
+              name="password"
+              {...register("password", {
+                required: "El password tiene que existir",
+                // pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/,
+              })}
+            />
+          </div>
         </div>
         <button>Login</button>
       </form>
