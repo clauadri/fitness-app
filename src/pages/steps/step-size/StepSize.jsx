a8f4ab873d889a5997829890a8c9460c57ea2abe
angelica <angelica.cavero@gmail.com>
2022-12-05 14:33:56 -0500

    styles fixed

--- src/pages/steps/step-size/StepSize.jsx
+++ src/pages/steps/step-size/StepSize.jsx
@@ -11,7 +11,7 @@ const StepSize = () => {
   const onSubmit = (data) => {
     //Recoge peso y altura
     console.log(data);
-    if(data.height > 1 && data.weight > 1){
+    if (data.height > 1 && data.weight > 1) {
       API.post("/stats/create", data);
       navigate("/step-problem-areas");
     }
@@ -19,14 +19,16 @@ const StepSize = () => {
 
   return (
     <div className="steps-container">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form className="form-container" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <div className="step-options">
             <h2>Introduce tu altura</h2>
           </div>
           <label>
             {" "}
-            <input min={130} max={260}
+            <input
+              min={130}
+              max={260}
               className="form-field"
               type="number"
               {...register("height")}
@@ -40,7 +42,9 @@ const StepSize = () => {
           </div>
           <label>
             {" "}
-            <input min={40} max={300}
+            <input
+              min={40}
+              max={300}
               className="form-field"
               type="number"
               {...register("weight")}
@@ -55,26 +59,20 @@ const StepSize = () => {
         <h3>Cómo evaluar su peso</h3>
         <p>
           Mucha grasa corporal puede provocar enfermedades relacionadas con el
-          peso y otros problemas de salud. 
-          
-          Tener bajo peso también es un riesgo
-          para la salud. 
-          </p>
-          <p>
-          El índice de masa corporal (IMC) y la circunferencia de
-          la cintura son herramientas de detección para estimar el estado de
-          peso en relación con el riesgo potencial de enfermedad. Sin embargo,
-          estas no son herramientas de diagnóstico para los riesgos de
-          enfermedad.
-          </p>
-           <p>
-           Un proveedor de atención médica capacitado debe realizar
-          otras evaluaciones de salud para evaluar el riesgo de enfermedad y
+          peso y otros problemas de salud. Tener bajo peso también es un riesgo
+          para la salud.
+        </p>
+        <p>
+          El índice de masa corporal (IMC) y la circunferencia de la cintura son
+          herramientas de detección para estimar el estado de peso en relación
+          con el riesgo potencial de enfermedad. Sin embargo, estas no son
+          herramientas de diagnóstico para los riesgos de enfermedad.
+        </p>
+        <p>
+          Un proveedor de atención médica capacitado debe realizar otras
+          evaluaciones de salud para evaluar el riesgo de enfermedad y
           diagnosticar el estado de la enfermedad.
-           </p>
-          
-          
-       
+        </p>
       </div>
     </div>
   );
